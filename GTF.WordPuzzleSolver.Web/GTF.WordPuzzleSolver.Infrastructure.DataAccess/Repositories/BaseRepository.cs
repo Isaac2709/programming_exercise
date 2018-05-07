57f43052412b54a8c17e14d635f592f28064a687
Unknown <isaac2709@hotmail.com>
2018-05-06 19:54:02 -0600

    Creating the base structure project according to the specification needed to the puzzle

---
+++ GTF.WordPuzzleSolver.Web/GTF.WordPuzzleSolver.Infrastructure.DataAccess/Repositories/BaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GTF.WordPuzzleSolver.Infrastructure.DataAccess.Repositories
+{
+    public abstract class BaseRepository
+    {
+        public virtual IList<TRequest> LoadData<TRequest>(string jsonPath)
+        {
+            using (StreamReader r = new StreamReader(jsonPath))
+            {
+                string json = r.ReadToEnd();
+                IList<TRequest> items = JsonConvert.DeserializeObject<IList<TRequest>>(json);
+
+                return items;
+            }
+        }
+    }
+}
